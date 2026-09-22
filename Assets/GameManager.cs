35cced89b47bb8d521915fd544f688ff13aacf7c
KevX36 <kevincard2007@gmail.com>
2026-09-22 20:42:50 -0300

    made levels

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel1()
+    {
+        
+    }
+    public void LoadLevel2()
+    {
+
+    }
+    public void LoadLevel3()
+    {
+
+    }
+}
