94d6c32191c0c023aacfc1dbbdf55e340c81724d
KevX36 <kevincard2007@gmail.com>
2026-09-22 22:11:49 -0300

    added save system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,29 +1,77 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         DontDestroyOnLoad(this);
         Load();
     }
     public stats Stats;
-    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     public void Load()
     {
-        Stats = FindAnyObjectByType<stats>();
+        if (Stats == null)
+        {
+            Stats = FindAnyObjectByType<stats>();
+        }
+        string path = Application.persistentDataPath + "/stats.save";
+        if (File.Exists(path))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
 
 
 
-        Stats.UpdateStatText();
+
+
+
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+
+            try
+            {
+                Stats = bf.Deserialize(stream) as stats;
+                Stats.UpdateStatText();
+            }
+            finally
+            {
+                stream.Close();
+            }
+
+            Debug.Log("loaded");
+
+        }
+        else { Debug.LogError("no save found"); }
+
+        
     }
     public void Save()
     {
+        if (Stats == null)
+        {
+            Stats = FindAnyObjectByType<stats>();
+        }
+        BinaryFormatter bf = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/stats.save";
+        FileStream stream = new FileStream(path, FileMode.Create);
 
+        stats data = Stats;
+
+        bf.Serialize(stream, data);
+        stream.Close();
+        Debug.Log("saved");
     }
     
     public void LoadLevel1()
